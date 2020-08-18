03f23c3fb6c2de9cc5d590c028bd58b3c1e935b8
Harsh97 <harsh97.au@gmail.com>
2020-08-18 22:41:44 +0930

    intial js file which import tutorialfile

---
+++ app.js
@@ -0,0 +1,7 @@
+//import tutorial from './tutorial';
+const tutorial = require('./module');
+//console.log(tutorial);
+//console.log(tutorial(10,10));
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.Math());
